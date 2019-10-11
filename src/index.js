aa802a1401f285b83348de2b5ccecec9780115d8
ose <leslieokoduwa@gmail.com>
2019-10-11 23:54:42 +0100

    Added a welcome message

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) => {
+  res.send({
+    Welcome: 'Welcome to the Task Manager Api!'
+  })
+})
+
 app.listen(PORT, () => {
   console.log('listening at port ' + PORT)
 })
